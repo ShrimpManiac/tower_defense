5886a6d78c9d3355e63a7ab18d9b50f0126b56df
tjdcjf1996 <r_louis@naver.com>
2024-10-11 12:50:19 +0900

    update: "계좌조회 기능 핸들러 매핑"
    클라이언트에서 계좌 조회를 위한 핸들러 매핑 추가

--- src/handlers/handlerMappings.js
+++ src/handlers/handlerMappings.js
@@ -1,12 +1,14 @@
 import { moveStageHandler } from './stage.handler.js';
 import { gameStart, gameEnd } from './game.handler.js';
 import { obtainItem } from './item.handler.js';
+import { checkBalanceAccount } from './account.handler.js';
 
 const handlerMappings = {
   2: gameStart,
   3: gameEnd,
   11: moveStageHandler,
   20: obtainItem,
+  100: checkBalanceAccount,
 };
 
 export default handlerMappings;
