2f28d26b32b91ba8a7ff749f07bd59860f74e772
tjdcjf1996 <r_louis@naver.com>
2024-10-12 23:46:22 +0900

    update: "stage 핸들러 매핑"
    기존 핸들러에서 스테이지 매핑 200번 추가

--- src/handlers/handlerMappings.js
+++ src/handlers/handlerMappings.js
@@ -1,6 +1,7 @@
 import { gameStart, gameEnd } from './game.handler.js';
 import { buyTower, sellTower, upgradeTower } from './tower.handler.js';
 import { checkBalanceAccount, hasSufficientBalance } from './account.handler.js';
+import { getCurrentStage } from './stage.handler.js';
 
 const handlerMappings = {
   2: gameStart,
@@ -10,6 +11,7 @@ const handlerMappings = {
   23: upgradeTower,
   100: checkBalanceAccount,
   101: hasSufficientBalance,
+  200: getCurrentStage,
 };
 
 export default handlerMappings;
