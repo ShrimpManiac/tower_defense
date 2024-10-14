2b225b798a110e6c9c995d8db96bf3ded4f4df86
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-15 00:40:08 +0900

    update: "deleteTower 함수 추가"
    - 주어진 towerId의 타워를 삭제하고 삭제한 타워를 반환함

--- src/models/tower.model.js
+++ src/models/tower.model.js
@@ -16,6 +16,20 @@ export const setTower = (uuid, tower) => {
   return towers[uuid].push(tower);
 };
 
+// 타워 삭제
+export const deleteTower = (uuid, towerId) => {
+  // 삭제할 타워의 인덱스 검색
+  const towerIndex = towers[uuid].findIndex((tower) => tower.id === towerId);
+  // 예외처리: 타워를 찾지 못함
+  if (towerIndex === -1) throw new Error(`Tower not found`);
+  // 삭제할 타워 저장
+  const deletedTower = towers[uuid][towerIndex];
+  // 타워 삭제
+  towers[uuid].splice(towerIndex, 1);
+  // 삭제한 타워 반환
+  return deletedTower;
+};
+
 // Id로 타워 찾기
 export function getTowerById(uuid, towerId) {
   if (!towers[uuid] || towers[uuid].length === 0) throw new Error(`User ${uuid} has no tower`);
