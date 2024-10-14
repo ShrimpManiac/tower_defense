8abac8cced0107de0f345dd0d8290488893da7af
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-14 15:50:06 +0900

    update: "getFirstAsset -> 애셋의 첫 항목을 가져오는 함수 추가"
    - getFirstAsset 함수 추가
    - 서버 로드시 애셋을 id순으로 정렬하도록 수정
    - 기존의 코드를 새로운 함수로 모듈화

--- client/utils/assets.js
+++ client/utils/assets.js
@@ -35,7 +35,7 @@ export const getGameAsset = (assetType) => {
  * 호출 예시: const monsterData = findAssetDataById(ASSET_TYPE.MONSTER, monsterId);
  * @param {ASSET_TYPE} assetType 조회할 게임에셋 타입
  * @param {number} id 조회할 항목의 id
- * @returns {JSON} 해당 id의 항목 ( 예시: { "id: 1001, hp: 50 "} )
+ * @returns {JSON} 해당 id의 항목 ( 예시: { "id: 3004, maxHp: 50, ..."} )
  */
 export const findAssetDataById = (assetType, id) => {
   switch (assetType) {
@@ -59,9 +59,33 @@ export const findAssetDataById = (assetType, id) => {
  * 
  * 호출 예시: const nextStage = getNextAsset(ASSET_TYPE.STAGE, stageId);
  * @param {ASSET_TYPE} assetType 조회할 게임에셋 타입
- * @param {number} id 현재 항목의 id
- * @returns {JSON} 다음 id의 항목 ( 예시: { "id: 1002, hp: 60 "} )
+ * @param {number} id 현재 항목의 id ( 예시: 4002 )
+ * @returns {JSON} 다음 id의 항목 ( 예시: { "id: 4003, monsterIds: [...], ..."} )
  */
 export const getNextAsset = (assetType, id) => {
   return findAssetDataById(assetType, id + 1);
+};
+
+/**
+ * 특정 게임에셋의 첫 항목을 조회하는 함수
+ *
+ * 호출 예시: const firstStage = getFirstAsset(ASSET_TYPE.STAGE);
+ * @param {ASSET_TYPE} assetType 조회할 게임에셋 타입
+ * @returns {JSON} 게임애셋의 첫 항목 ( 예시: { "id: 4001, monsterIds: [...], ..."} )
+ */
+export const getFirstAsset = (assetType) => {
+  switch (assetType) {
+    case ASSET_TYPE.STAGE:
+      return stageData.data[0].id;
+    case ASSET_TYPE.MONSTER:
+      return monsterData.data[0].id;
+    case ASSET_TYPE.TOWER:
+      return towerData.data[0].id;
+    case ASSET_TYPE.TOWER_SKILL:
+      return towerSkillData.data[0].id;
+    case ASSET_TYPE.PATH:
+      return pathData.data[0].id;
+    default:
+      throw new Error('Invalid asset type: ' + assetType);
+  }
 };
