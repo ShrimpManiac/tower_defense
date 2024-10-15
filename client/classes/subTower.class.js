aedf1559dbeaf2c1865c924ac215304a6d8a5b57
rlaguswls2 <hydrated200ml@gmail.com>
2024-10-16 03:26:44 +0900

    fix: "import 주소 수정 및 draw함수 추가, beamDuration 초기화 추가, 멀티타워의 공격함수 오버라이딩 삭제"

--- client/classes/subTower.class.js
+++ client/classes/subTower.class.js
@@ -1,4 +1,4 @@
-import { Tower } from '../tower.class.js';
+import { Tower } from './tower.class.js';
 
 /**
  * 일반 타워 클래스
@@ -15,6 +15,20 @@ export class NormalTower extends Tower {
 export class SlowTower extends Tower {
   constructor(assetId, instanceId, spawnLocation) {
     super(assetId, instanceId, spawnLocation);
+  }
+
+  draw(ctx, towerImage) {
+    ctx.drawImage(towerImage, this.x, this.y, this.width, this.height);
+    if (this.remainingBeamDuration > 0 && this.target) {
+      ctx.beginPath();
+      ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
+      ctx.lineTo(this.target.x + this.target.width / 2, this.target.y + this.target.height / 2);
+      ctx.strokeStyle = 'green';
+      ctx.lineWidth = 10;
+      ctx.stroke();
+      ctx.closePath();
+      this.remainingBeamDuration--;
+    }
   }
 
   attack(monster) {
@@ -36,6 +50,7 @@ export class SlowTower extends Tower {
       }
 
       this.cooldownLeft = this.cooldown; // 공격 후 쿨타임 초기화
+      this.remainingBeamDuration = 30;
     }
   }
 }
@@ -43,31 +58,5 @@ export class SlowTower extends Tower {
 export class MultiTower extends Tower {
   constructor(assetId, instanceId, spawnLocation) {
     super(assetId, instanceId, spawnLocation);
-  }
-
-  attack(monster) {
-    if (this.cooldownLeft <= 0) {
-      this.target = monster;
-
-      // 몬스터 이동 속도 감소 적용
-      if (monster.speed && !monster.isSlowed) {
-        monster.speed *= this.skillValue; // 몬스터의 이동 속도를 감소시킴
-        monster.isSlowed = true; // 슬로우 효과를 받고 있음을 표시
-
-        // 일정 시간 후 몬스터의 속도 원상 복구
-        this.applySkillEffect(monster);
-      }
-
-      this.cooldownLeft = this.cooldown; // 공격 후 쿨타임 초기화
-    }
-  }
-
-  applySkillEffect(monster) {
-    setTimeout(() => {
-      if (monster.isSlowed) {
-        monster.speed /= this.skillValue;
-        monster.isSlowed = false;
-      }
-    }, this.skillDuration * 1000); // 초 단위로
   }
 }
