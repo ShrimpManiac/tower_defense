25f4cfc313b48cd35507c17f57998e25c86f986c
tjdcjf1996 <r_louis@naver.com>
2024-10-16 01:02:56 +0900

    fix: "소켓 연결 시 uuid 부여 방식으로 수정"
    기존 로그인만 구현되어있던 방식을 소켓 연결 시 개인 UUID를 통하여 등록할 수 있도록 코드 수정

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,7 +9,7 @@ export const addUser = (user) => {
 // 유저 삭제
 export const removeUser = (socketId) => {
   const index = users.findIndex((user) => user.socketId === socketId);
-  if (index != -1) {
+  if (index !== -1) {
     return users.splice(index, 1)[0];
   }
 };
